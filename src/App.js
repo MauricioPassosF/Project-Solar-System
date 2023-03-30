104cd15886d1f3986be1b5d29f56ec01ea4ac3c1
MauricioPassosF <mauricio.passos97@gmail.com>
2023-03-30 15:57:39 -0300

    Requisito 7

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import Header from './components/Header';
 import SolarSystem from './components/SolarSystem';
+import Missions from './components/Missions';
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        <p>Sistema Solar</p>
         <Header />
         <SolarSystem />
+        <Missions />
       </div>
     );
   }
